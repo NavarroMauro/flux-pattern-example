2cd3078e54d848022a22dc9371e3d3e8b13c378a
Mauricio Cifuentes <mcifuent@gmail.com>
2019-10-06 13:30:52 +0200

    project finished and waiting for improvements

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,7 +6,9 @@ export function saveCourse(course) {
   return courseApi.saveCourse(course).then(savedCourse => {
     // Hey dispatcher, go and tell all the stores that a new course was created.
     dispatcher.dispatch({
-      actionType: actionTypes.CREATE_COURSE,
+      actionType: course.id
+        ? actionTypes.UPDATE_COURSE
+        : actionTypes.CREATE_COURSE,
       course: savedCourse
     });
   });
@@ -16,7 +18,16 @@ export function loadCourses() {
   return courseApi.getCourses().then(courses => {
     dispatcher.dispatch({
       actionType: actionTypes.LOAD_COURSES,
-      course: courses
+      courses: courses
+    });
+  });
+}
+
+export function deleteCourse(id) {
+  return courseApi.deleteCourse(id).then(() => {
+    dispatcher.dispatch({
+      actionType: actionTypes.DELETE_COURSE,
+      id: id
     });
   });
 }
